a99a40e921c0e7216e1aeb5858df9b75127efbff
Lawrence <lawrenccee@gmail.com>
2018-03-01 08:55:11 -0800

    7 without using strings

--- 7_reverse_integer.rb
+++ 7_reverse_integer.rb
@@ -1,16 +1,39 @@
 # @param {Integer} x
 # @return {Integer}
+# def reverse(x)
+#   boundaries = [-2**31, 2**31-1]
+#   negative = x < 0 ? -1 : 1
+  
+#   reversed = x.abs.to_s.chars.reverse
+  
+#   result = (reversed.join.to_i) * negative
+  
+#   return 0 if result < boundaries.first || result > boundaries.last
+  
+#   result
+# end
+
+# PROBABLY A BETTER WAY TO DO THIS
+
+# USING NO STRING
+
 def reverse(x)
   boundaries = [-2**31, 2**31-1]
   negative = x < 0 ? -1 : 1
-  
-  reversed = x.abs.to_s.chars.reverse
-  
-  result = (reversed.join.to_i) * negative
-  
+  x = x.abs 
+  result = 0
+
+  while x > 0
+    result *= 10
+    result += x % 10
+    x /= 10
+  end
+
   return 0 if result < boundaries.first || result > boundaries.last
   
-  result
+  result * negative
 end
 
-# PROBABLY A BETTER WAY TO DO THIS
+p reverse(-321)
+p reverse(123)
+p reverse(12345678901231243)
