9549c32e15b4aab15d0c239fd8d5c74e67ecec45
Lawrence <lawrenccee@gmail.com>
2018-03-19 18:48:46 -0700

    iteratively climb stairs

--- 70_climb_stairs.rb
+++ 70_climb_stairs.rb
@@ -1,9 +1,22 @@
 # @param {Integer} n
 # @return {Integer}
-def climb_stairs(n, cache = { 0 => 0, 1 => 1, 2 => 2})
-  return cache[n] if cache[n]
+# def climb_stairs(n, cache = { 0 => 0, 1 => 1, 2 => 2})
+#   return cache[n] if cache[n]
   
-  ans = climb_stairs(n-1, cache) + climb_stairs(n-2, cache)
-  cache[n] = ans
-  ans
+#   ans = climb_stairs(n-1, cache) + climb_stairs(n-2, cache)
+#   cache[n] = ans
+#   ans
+# end
+
+# how would i do this iteratively?
+def climb_stairs(n)
+  result = [0, 1, 2]
+  
+  return result[n] if n <= 2
+  
+  until result.length == n + 1
+    result.push(result[-1] + result[-2])
+  end
+    
+  result[n]
 end
