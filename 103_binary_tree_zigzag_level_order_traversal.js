adc8d38ba476f33c5d253a9a7c6acc77e30e7f6d
Lawrence <lawrenccee@gmail.com>
2018-04-02 23:05:47 -0700

    binary tree zigzag level order traversal

---
+++ 103_binary_tree_zigzag_level_order_traversal.js
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var zigzagLevelOrder = function (root) {
+  if (!root) {
+    return [];
+  }
+
+  let result = [];
+  let subArr = [root.val];
+  let lastRow = [root]
+  let flip = true;
+
+  while (lastRow.length > 0) {
+    result.push(subArr);
+
+    subArr = new Array;
+
+    if (!flip) {
+      lastRow = lastRow.reverse();
+    }
+
+    for (let i = 0; i < lastRow.length; ++i) {
+      if (lastRow[i].left) {
+        subArr.push(lastRow[i].left);
+      }
+      if (lastRow[i].right) {
+        subArr.push(lastRow[i].right);
+      }
+    }
+
+    if (flip) {
+      subArr = subArr.reverse();
+    }
+
+    lastRow = subArr;
+    subArr = subArr.map((el) => el.val);
+
+    flip = !flip;
+  }
+
+  return result;
+};
