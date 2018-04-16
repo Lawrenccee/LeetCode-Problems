72acab2e6147d8e4345e63ae59a0416f333be9f3
Lawrence <lawrenccee@gmail.com>
2018-04-16 16:44:22 -0700

    valid parentheses

---
+++ valid_parentheses.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+import java.util.HashMap;
+
+class valid_parentheses {
+  public boolean isValid(String s) {
+    HashMap<String, String> mapping = new HashMap<String, String>();
+    mapping.put(")", "(");
+    mapping.put("]", "[");
+    mapping.put("}", "{");
+
+    Stack<String> stack = new Stack<String>();
+
+    for (int i = 0; i < s.length(); i++) {
+      String curr = s.substring(i, i + 1);
+      if (mapping.get(curr) != null) {
+        if (stack.empty() || !mapping.get(curr).equals(stack.peek())) {
+          return false;
+        } else {
+          stack.pop();
+        }
+      } else {
+        stack.push(curr);
+      }
+    }
+
+    return stack.empty();
+  }
+}
